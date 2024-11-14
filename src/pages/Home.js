db2cb4ce2e3915c279bb04e48c631dec0c27e788
hiqram <hasan.iqramlexutb.se>
2024-11-14 22:45:56 +0100

    Testimonials

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
-
+import Testimonials from '../components/Testimonials'
 
 function Home() {
   return (
     <div>
       <HeroSection/>
-      <p>Hej!</p>
+      <Testimonials/>
+      
     </div>
   )
 }
