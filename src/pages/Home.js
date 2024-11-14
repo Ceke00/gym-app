6f783b574f3d12818b13621be635fb2cabf8f3bf
Cecilia Egevad <ceciliaegevad@gmail.com>
2024-11-14 11:50:31 +0100

    Cleaned code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,12 @@ import CardSection from "../components/CardSection";
 import HalfSection from "../components/HalfSection";
 import Testimonials from "../components/Testimonials";
 import "./Home.scss";
-import { Container } from "react-bootstrap";
 
 function Home() {
   return (
     <div>
       <HeroSection />
       <CardSection />
-      {/* <Container fixed fluid className="my-5" id="bg-image"/> */}
       <div id="bg-image" className="my-5"></div>
       <HalfSection />
       <Testimonials />
