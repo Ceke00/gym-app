cafd8037bde359151f38bc0193a6b87ba25f5163
Cecilia Egevad <ceciliaegevad@gmail.com>
2024-11-14 11:49:37 +0100

    HalfSection done. Bg-image set on HomePage

--- src/components/HalfSection.js
+++ src/components/HalfSection.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import data from "../data/home.json";
+import girl from "../images/pexels-pixabay-416778.jpg"; 
 
 function HalfSection() {
+   const [content, setContent] = useState({
+     text: { heading: "", paragraphs: [] },
+     alt: "",
+   });
+
+   useEffect(() => {
+     if (data.half) {
+       setContent(data.half);
+     }
+   }, []);
+
   return (
-    <div>
-      half section
-    </div>
-  )
+    <Container fluid="lg" className="half-section">
+      <Row className="d-flex flex-column flex-md-row">
+        <Col md={6} className="order-md-2">
+          <img src={girl} alt={content.alt} className="img-fluid" />
+        </Col>
+        <Col md={6} className="order-md-1">
+          <h2>{content.text.heading}</h2>
+          {content.text.paragraphs.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default HalfSection
+export default HalfSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection'
-
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import CardSection from "../components/CardSection";
+import HalfSection from "../components/HalfSection";
+import Testimonials from "../components/Testimonials";
+import "./Home.scss";
+import { Container } from "react-bootstrap";
 
 function Home() {
   return (
     <div>
-      <HeroSection/>
-      <p>Hej!</p>
+      <HeroSection />
+      <CardSection />
+      {/* <Container fixed fluid className="my-5" id="bg-image"/> */}
+      <div id="bg-image" className="my-5"></div>
+      <HalfSection />
+      <Testimonials />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
