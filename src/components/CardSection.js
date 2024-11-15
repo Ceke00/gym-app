07cbf46e2707f6e521a430ee08ed2207787b73de
al-sw <alanjunior.sweeney@gmail.com>
2024-11-15 12:15:44 +0100

    manual merge card section

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card"; // Importing the Card component
+
+import image1 from "../images/pexels-823sl-2294361.jpg";
+import image2 from "../images/pexels-pixabay-416778.jpg";
+import image3 from "../images/pexels-pixabay-209983.jpg";
 
 function CardSection() {
+  // Define an array of card data, passing imported images
+  const cardData = [
+    { id: 1, image: image1, title: "Strength training" }, // Pass imported image1
+    { id: 2, image: image2, title: "Hopscotch hiit" }, // Pass imported image2
+    { id: 3, image: image3, title: "Yoga" }, // Pass imported image2
+  ];
+
   return (
-    <div>
-      card section
+    <div className="container">
+      <div className="cardsSection row ms-auto me-auto p-5">
+        <h2 className="text-left mb-2">New classes</h2>
+        {cardData.map((data) => (
+          <div key={data.id} className="col-md-4 card">
+            <Card image={data.image} title={data.title} />{" "}
+            {/* Passing the correct props */}
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default CardSection
+export default CardSection;
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../custom.scss";
+import "../components/login.scss";
 
 function LoginPage() {
   return (
-    <div>
-      LoginPage
+    <div className="container">
+      <div className="row">
+        <div className="md-col-8 me-auto ms-auto">
+          <h2>Login</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <a href="#">
+              <p className="text-white linkSignup">
+                Not a member already? - become a member here
+              </p>
+            </a>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
