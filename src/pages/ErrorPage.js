18aaa73a0d2abc578b2f9a30b4b63cb716b0e9f1
Cecilia Egevad <ceciliaegevad@gmail.com>
2024-11-19 14:28:10 +0100

    added error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Container} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import "./ErrorPage.scss";
 
 function ErrorPage() {
   return (
-    <div>
-      Error Page
-    </div>
-  )
+    <Container className="my-5">
+      <h1>Oops!</h1>
+      <p>Something went wrong.</p>
+      <p>
+        Back to{" "}
+        <Link className="text-light link-offset-2 link" to="/">
+          Home Page
+        </Link>{" "}
+      </p>
+    </Container>
+  );
 }
 
 export default ErrorPage
